72914e5ad103b589914fa5f7d89d7aaa224bddce
Tuomas Kauppinen <tuomaskaup@hotmail.com>
2020-12-06 19:23:35 +0200

    Added few tests to isDate.test.ts.

--- tests/isDate.test.ts
+++ tests/isDate.test.ts
@@ -3,6 +3,12 @@ import isDate from '../src/isDate.js';
 it('works with date values', () => {
     expect(isDate(new Date)).toEqual(true);
     expect(isDate('Mon April 23 2012')).toEqual(false);
+    expect(isDate('12/24/2020')).toEqual(false);
+    expect(isDate('24/12/2020')).toEqual(false);
+    expect(isDate('12-24-2020')).toEqual(false);
+    expect(isDate('23-12-2020')).toEqual(false);
+    expect(isDate('11122020')).toEqual(false);
+    expect(isDate(11122020)).toEqual(false);
 });
 
 it('works with null and undefined values', () => {
